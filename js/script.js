f082f8602ec6676cbdf572eb51777dd20ffbaa31
Ivan Kiziuk <ivanmnksos@gmail.com>
2022-12-03 22:45:34 +0200

    lesson_1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,37 @@
-const result = "Hello!";
-console.log(result);
+// const result = "Hello!";
+// console.log(result);
+
+// const value = prompt("Please enter a number!");
+// console.log(typeof value); // "string"
+// console.log(value); // "5"
+
+// let counter = 0;
+// while (counter < 10) {
+//     console.log("counter: ", counter);
+//     counter += 1;
+//   }
+
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 1;
+// }
+
+// let password = "";
+
+// do {
+//   password = prompt("Введіть пароль довший 4-х символів", "");
+// } while (password.length < 5);
+
+// console.log("Ввели пароль: ", password);
+
+// const target = 5;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1) {
+//   sum += i;
+// }
+
+// console.log(sum);
